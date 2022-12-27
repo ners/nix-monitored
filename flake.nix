f7dec2da402112c20bd069582bd4e0b7bc81ce0b
Bernardo Meurer <bernardo@meurer.org>
2022-12-27 12:06:21 -0300

    refactor(flake): use nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     flake-utils.url = github:numtide/flake-utils;
   };
 
